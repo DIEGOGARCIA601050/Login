52dcda4256239702a622713986ba3a3b3c1b8415
DiegoGRoldán <ekrnal9@gmail.com>
2023-10-29 18:42:48 -0600

    añadimos estilos + añadimos base de datos para futura comunicación + agregamos inp:contraseña

--- login.js
+++ login.js
@@ -1,10 +1,14 @@
-console.log('funciona');
-let Cantidad;
+
 document.querySelector('form')
-  .addEventListener('submit', e => {
-    e.preventDefault()
-    const data = Object.fromEntries(
-      new FormData(e.target)
-    )
-    alert(JSON.stringify(data))
-  })
+    .addEventListener('submit', e => {
+        let Cantidad;
+        e.preventDefault()
+        const data = Object.fromEntries(
+        new FormData(e.target)
+        )
+        alert(JSON.stringify(data))
+        Cantidad = JSON?.stringify(data)
+        Cantidad = JSON.parse(Cantidad)
+        console.info(Cantidad)
+        return Cantidad
+    })
