99f748d906d875872edbb967d851b5a1b6c2ae75
DiegoGRoldán <ekrnal9@gmail.com>
2023-11-15 22:31:20 -0600

    perfeccionamos el servidor con express, importamos el modulo path

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,22 @@
-const http = require('node:http')
-const fs = require('node:fs')
+// const http = require('node:http')
+// const fs = require('node:fs')
 
-const server = http.createServer((req, res)=>{
-    const read = fs.createReadStream('../index.html')
-    read.pipe(res)
-})
-
-server.listen(3000)
-console.log(`Server on port ${3000}`);
-// const express = require('express')
-
-// const app = express()
-
-// app.get('/',(req, resp)=>{
-//     resp.sendFile('../index.html', {
-//         root:__dirname
-//     })
+// const server = http.createServer((req, res)=>{
+//     const read = fs.createReadStream('../index.html')
+//     read.pipe(res)
 // })
 
-// app.listen(3000)
-// console.log(`Server on port ${3000}`)
+// server.listen(3000)
+// console.log(`Server on port ${3000}`);
+let path = require('path');
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req, resp)=>{
+    resp.sendFile(path.resolve('../index.html'));
+
+})
+
+app.listen(3000)
+console.log(`Server on port ${3000}`)
