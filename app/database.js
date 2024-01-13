ba5a13395928f1effe58ae2d6d456251e05c79ed
DiegoGRoldán <123426763+DIEGOGARCIA601050@users.noreply.github.com>
2024-01-13 03:21:04 +0000

    Implementamos el patron Singleton

--- app/database.js
+++ app/database.js
@@ -1,14 +1,36 @@
 // Obtém o cliente
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
+class Config {
+  constructor(host, port, user, password, database) {
+    if(Config.instance) {
+      return Config.instance
+    } else {
+    Config.instance = this
+    this.host = host,
+    this.port = port,
+    this.user = user,
+    this.password = password,
+    this.database = database
+  }
+  }
+}
+const config = new Config('localhost', 3306, 'root', 'sqllentesedmundo', 'usuarios');
 // Cria a conexão com o Banco de Dados
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'sqllentesedmundo',
-  database: 'usuarios'
-});
+Object.freeze(config)
+const config2 = new Config('localhost', 3532, 'root', '13455', 'registrados');
+async function ConectionToDB(conf) {
+  const connection = await mysql.createConnection(conf);
+  try {
+    console.log('Listo');
+    return connection
+  } catch(error) {
+    console.log(`Error: ${error}`);
+    return false
+  }
+}
+
+const connection = ConectionToDB(config)
 
 module.exports = {
   connection
